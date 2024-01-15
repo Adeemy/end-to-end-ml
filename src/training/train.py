a7386f6e178a769151401bd5c6421519a3f48101
mohammed.aladeemy <mohammed.aladeemy@outlook.com>
2024-01-15 20:23:45 +0000

    updated docs and raised error when model cannot be deployed

--- src/training/train.py
+++ src/training/train.py
@@ -442,7 +442,8 @@ def main(config_yaml_abs_path: str, comet_api_key: str, artifacts_dir: PosixPath
 
     # Log and register champion model (in Comet, model must be logged first)
     # Note: the best model should not be deployed in production if its score
-    # on the test set is below minimum score.
+    # on the test set is below minimum score. Otherwise, prevent deploying 
+    # the model by raising error preventing build job.
     BEST_MODEL_TEST_SCORE = test_scores.get(f"test_{COMPARISON_METRIC}")
     if BEST_MODEL_TEST_SCORE >= DEPLOYMENT_SCORE_THRESH:
         log_and_register_champ_model(
@@ -451,6 +452,8 @@ def main(config_yaml_abs_path: str, comet_api_key: str, artifacts_dir: PosixPath
             pipeline=best_model_pipeline,
             exp_obj=best_model_exp_obj,
         )
+    else:
+        raise ValueError(f"Best model score is {BEST_MODEL_TEST_SCORE}, which is lower than deployment threshold {DEPLOYMENT_SCORE_THRESH}.")
 
 
 ###########################################################
