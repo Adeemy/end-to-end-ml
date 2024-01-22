762049fdfb62d8d504bc5be170423ad3fbc439cf
Mohammed Aladeemy, PhD <mohammed.aladeemy@outlook.com>
2024-01-22 04:17:17 +0000

    test setting paths

--- set_path.sh
+++ set_path.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e 'export PYTHONPATH=$PYTHONPATH:/src/' >> ~/.bashrc
+echo -e 'export PYTHONPATH=$PYTHONPATH:./src/' >> ~/.bashrc
 # echo -e 'export PYTHONPATH=$PYTHONPATH:./src/training/utils:./src/config:./src/feature_store/utils:./src/feature_store/feature_repo/data' >> ~/.bashrc
