b15aab04eb7b47df6089157e19d2cddefb6199ea
Mohammed Aladeemy, PhD <mohammed.aladeemy@outlook.com>
2024-01-24 00:15:52 +0000

    removed __init__.py from data folder

--- src/feature_store/prep_data.py
+++ src/feature_store/prep_data.py
@@ -13,8 +13,6 @@ from src.config.path import DATA_DIR
 from src.feature_store.utils.config import Config
 from src.feature_store.utils.prep import DataPreprocessor, DataTransformer
 
-print(sys.path)
-
 
 #################################
 def main(config_yaml_abs_path: str, data_dir: PosixPath):
