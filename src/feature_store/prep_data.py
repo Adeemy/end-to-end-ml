33a0d54ceb3bf21e66306e44e178716768660373
Mohammed Aladeemy, PhD <mohammed.aladeemy@outlook.com>
2024-07-05 05:06:07 +0000

    refactored prep_data.py

--- src/feature_store/prep_data.py
+++ src/feature_store/prep_data.py
@@ -16,8 +16,7 @@ from src.utils.logger import get_console_logger
 from src.utils.path import DATA_DIR
 
 
-#################################
-def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> None:
+def import_data(config_yaml_path: str, data_dir: PosixPath) -> pd.DataFrame:
     """Imports raw data to be preprocessed and transformed.
 
     Args:
@@ -25,15 +24,10 @@ def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> 
         data_dir (PosixPath): path to the data directory.
 
     Returns:
-        None.
+        pd.DataFrame: raw dataset.
     """
 
-    logger.info(
-        f"Directory of data perprocessing and transformation config file: {config_yaml_path}"
-    )
-
-    # Get initiated FeatureStore
-    # Note: repo_path is the relative path to where this script is located.
+    # Get configuration parameters
     config = Config(config_path=config_yaml_path)
     pk_col_name = config.params["data"]["pk_col_name"]
     class_column_name = config.params["data"]["class_col_name"]
@@ -41,28 +35,10 @@ def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> 
     datetime_col_names = config.params["data"]["datetime_col_names"]
     num_col_names = config.params["data"]["num_col_names"]
     cat_col_names = config.params["data"]["cat_col_names"]
-    feature_mappings = config.params["feature_mappings"]
-    class_mappings = config.params["class_mappings"]
-
-    event_timestamp_col_name = config.params["data"]["event_timestamp_col_name"]
     raw_dataset_file_name = config.params["files"]["raw_dataset_file_name"]
-    preprocessed_dataset_features_file_name = config.params["files"][
-        "preprocessed_data_features_file_name"
-    ]
-    preprocessed_dataset_target_file_name = config.params["files"][
-        "preprocessed_data_target_file_name"
-    ]
 
-    #################################
     # Import raw dataset
     raw_dataset = pd.read_parquet(path=data_dir / raw_dataset_file_name)
-
-    #################################
-    # Apply required preprocessing on raw dataset
-    # Note: preprocessing and transofmration stepd applied
-    # here include mapping values and defining column data
-    # types, i.e., doesn't cause data leakage. Hence,
-    # transformed dataset can be split into train and test sets.
     required_input_col_names = (
         [pk_col_name]
         + date_col_names
@@ -73,7 +49,29 @@ def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> 
     )
     raw_dataset = raw_dataset[required_input_col_names].copy()
 
-    #################################
+    return raw_dataset
+
+
+def preprocess_data(raw_dataset: pd.DataFrame, config_yaml_path: str) -> pd.DataFrame:
+    """Preprocesses raw data.
+
+    Args:
+        raw_dataset (pd.DataFrame): raw dataset.
+        config_yaml_path (str): path to the config yaml file.
+
+    Returns:
+        preprocessed_dataset (pd.DataFrame): preprocessed dataset.
+        data_preprocessor (DataPreprocessor): data preprocessor object.
+    """
+
+    # Get configuration parameters
+    config = Config(config_path=config_yaml_path)
+    pk_col_name = config.params["data"]["pk_col_name"]
+    date_col_names = config.params["data"]["date_col_names"]
+    datetime_col_names = config.params["data"]["datetime_col_names"]
+    num_col_names = config.params["data"]["num_col_names"]
+    cat_col_names = config.params["data"]["cat_col_names"]
+
     data_preprocessor = DataPreprocessor(
         input_data=raw_dataset,
         primary_key_names=[pk_col_name],
@@ -89,6 +87,31 @@ def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> 
     data_preprocessor.remove_duplicates_by_primary_key()
     data_preprocessor.specify_data_types()
     preprocessed_dataset = data_preprocessor.get_preprocessed_data()
+
+    return preprocessed_dataset, data_preprocessor
+
+
+def transform_data(
+    preprocessed_dataset: pd.DataFrame,
+    data_preprocessor: DataPreprocessor,
+    config_yaml_path: str,
+) -> pd.DataFrame:
+    """Transforms preprocessed data by mapping values and enriching data.
+
+    Args:
+        preprocessed_dataset (pd.DataFrame): preprocessed dataset.
+        data_preprocessor (DataPreprocessor): data preprocessor class instance.
+        config_yaml_path (str): path to the config yaml file.
+
+    Returns:
+        pd.DataFrame: transformed dataset.
+    """
+
+    # Get configuration parameters
+    config = Config(config_path=config_yaml_path)
+    class_column_name = config.params["data"]["class_col_name"]
+    feature_mappings = config.params["feature_mappings"]
+    class_mappings = config.params["class_mappings"]
 
     # Apply mapping on categorical features (e.g., general health level 3 to "Good")
     data_transformer = DataTransformer(
@@ -117,25 +140,84 @@ def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> 
             mapping_values=class_mappings["class_values"],
         )
 
-    preprocessed_data = data_transformer.preprocessed_data
+    transformed_data = data_transformer.preprocessed_data
+
+    return transformed_data
+
+
+#################################
+def main(config_yaml_path: str, data_dir: PosixPath, logger: logging.Logger) -> None:
+    """Imports raw data to be preprocessed and transformed.
+
+    Args:
+        config_yaml_path (str): path to the config yaml file.
+        data_dir (PosixPath): path to the data directory.
+        logger (logging.Logger): logger object.
+
+    Returns:
+        None.
+    """
+
+    logger.info(
+        "Directory of data perprocessing and transformation config file: %s",
+        config_yaml_path,
+    )
+
+    # Get configuration parameters
+    config = Config(config_path=config_yaml_path)
+    pk_col_name = config.params["data"]["pk_col_name"]
+    class_column_name = config.params["data"]["class_col_name"]
+    event_timestamp_col_name = config.params["data"]["event_timestamp_col_name"]
+
+    preprocessed_dataset_features_file_name = config.params["files"][
+        "preprocessed_data_features_file_name"
+    ]
+    preprocessed_dataset_target_file_name = config.params["files"][
+        "preprocessed_data_target_file_name"
+    ]
+
+    # Import raw dataset
+    raw_dataset = import_data(config_yaml_path=config_yaml_path, data_dir=data_dir)
+
+    logger.info("Raw dataset was imported.")
+
+    #################################
+    # Apply required preprocessing on raw dataset
+    # Note: preprocessing and transofmration stepd applied
+    # here include mapping values and defining column data
+    # types, i.e., doesn't cause data leakage. Hence,
+    # transformed dataset can be split into train and test sets.
+    preprocessed_dataset, data_preprocessor = preprocess_data(
+        raw_dataset=raw_dataset, config_yaml_path=config_yaml_path
+    )
+    logger.info("Raw dataset was preprocessed.")
+
+    transformed_data = transform_data(
+        preprocessed_dataset=preprocessed_dataset,
+        data_preprocessor=data_preprocessor,
+        config_yaml_path=config_yaml_path,
+    )
+    logger.info("Preprocessed dataset was transformed.")
 
     # Save features and target in a separate parquet files
     # Note: this is meant for patient entity in feature store.
-    preprocessed_features = raw_dataset.drop([class_column_name], axis=1, inplace=False)
+    preprocessed_features = transformed_data.drop(
+        [class_column_name], axis=1, inplace=False
+    )
     preprocessed_features[event_timestamp_col_name] = datetime.now()
     preprocessed_features.to_parquet(
         data_dir / preprocessed_dataset_features_file_name,
         index=False,
     )
 
-    preprocessed_target = preprocessed_data[[pk_col_name] + [class_column_name]].copy()
+    preprocessed_target = transformed_data[[pk_col_name] + [class_column_name]].copy()
     preprocessed_target[event_timestamp_col_name] = datetime.now()
     preprocessed_target.to_parquet(
         data_dir / preprocessed_dataset_target_file_name,
         index=False,
     )
 
-    logger.info("Preprocessed features and target were saved in feature store.")
+    logger.info("Preprocessed features and target were created.")
 
 
 ###########################################################
