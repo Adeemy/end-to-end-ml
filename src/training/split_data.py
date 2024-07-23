eee4e0d1332fadeca4bcd857d549d4e9ee4119f0
Mohammed Aladeemy, PhD <mohammed.aladeemy@outlook.com>
2024-07-23 04:15:25 +0000

    testing make setup_feast in Azure compute cluster

--- src/training/split_data.py
+++ src/training/split_data.py
@@ -16,7 +16,7 @@ from feast import FeatureStore
 from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
 
 # To import modules from the parent directory in Azure compute cluster
-root_dir = Path(__name__).resolve().parent.parent
+root_dir = Path(__name__).resolve().parent.parent.parent
 if str(root_dir) not in sys.path:
     sys.path.append(str(root_dir))
 
--- src/training/train.py
+++ src/training/train.py
@@ -27,7 +27,7 @@ from sklearn.preprocessing import (
 from xgboost import XGBClassifier
 
 # To import modules from the parent directory in Azure compute cluster
-root_dir = Path(__name__).resolve().parent.parent
+root_dir = Path(__name__).resolve().parent.parent.parent
 if str(root_dir) not in sys.path:
     sys.path.append(str(root_dir))
 
