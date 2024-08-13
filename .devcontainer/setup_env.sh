1d0bd83614618506bb34dc3725c62a5bf535051d
Mohammed Aladeemy, PhD <mohammed.aladeemy@outlook.com>
2024-08-13 03:13:15 +0000

    updated train env

--- .devcontainer/setup_env.sh
+++ .devcontainer/setup_env.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-# Setup default virtual environment and install requirements
-# echo -e 'export PYTHONPATH=$PYTHONPATH:~/' >> ~/.bashrc
-# python3 -m venv .venv
-# echo 'source .venv/bin/activate' >> ~/.bashrc
-# pip install --upgrade pip
-# pip install -r requirements.txt
+# Set up the default conda environment and install dependencies
+conda env create -f src/training/train-env.yml
+echo -e 'conda activate train-env' >> ~/.bashrc
 
-# # Install conda and create the environment
-# conda env create -f train-conda.yml
-# conda activate train-env
-
-# To remove the deprecation warning from jupyter_client when running tests
+# Remove deprecation warning from jupyter_client when running tests
 touch pytest.ini
 echo -e '[pytest]\nfilterwarnings ='\
 '\n    ignore::DeprecationWarning:jupyter_client.*'\
 '\n    ignore::optuna.exceptions.ExperimentalWarning'\
-'\n    ignore::DeprecationWarning:comet_ml.*' > pytest.ini
 
-# Install auxiliar tools
-sudo apt-get update
-sudo apt-get install tree # To print project structure
+# Install auxiliary tools
+sudo apt-get update && apt-get install -y \
+    tree # To print directory tree
